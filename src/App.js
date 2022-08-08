38209b3aedb28a218a155b2cc4200ef7ed75e2c4
carolinedam <61717208+carolinedam@users.noreply.github.com>
2022-08-08 16:47:48 -0400

    Set up logo and added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>We're building a dictionary app!</p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="App-link btn btn-primary"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Let's Go
         </a>
       </header>
     </div>
