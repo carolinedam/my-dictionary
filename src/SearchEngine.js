8de0e20dc88292c39b04a06f0a95a939bf74b4d9
carolinedam <61717208+carolinedam@users.noreply.github.com>
2022-08-11 20:26:30 -0400

    Lesson 6 / new components for results & meaning

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./SearchEngine.css";
 
 export default function SearchEngine() {
   let [word, setWord] = useState(null);
+  let [results, setResults] = useState(null);
 
   // documentation: https://dictionaryapi.dev/
 
   function displayWord(response) {
-    console.log(response.data[0].word);
+    setResults(response.data[0]);
   }
 
   function handleSubmit(event) {
@@ -26,6 +28,7 @@ export default function SearchEngine() {
       <form className="Searchform" onSubmit={handleSubmit}>
         <input type="text" autoFocus="on" onChange={updateWord}></input>
         <input type="submit" value="Search"></input>
+        <Results results={results} />
       </form>
     </div>
   );
