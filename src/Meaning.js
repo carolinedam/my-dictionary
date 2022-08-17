a445279d68af55f40eaa48d80361ee6794e31c71
carolinedam <61717208+carolinedam@users.noreply.github.com>
2022-08-17 16:00:22 -0600

    Lesson 3 / added phonetics

--- src/Meaning.js
+++ src/Meaning.js
@@ -13,9 +13,9 @@ export default function Meaning(props) {
               <strong>Definition: </strong>
               {definition.definition}
               <br />
-              <strong>Example: </strong>
               <em>{definition.example}</em>
               <br />
+
               <Synonyms synonyms={definition.synonyms} />
             </div>
           </div>
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -3,11 +3,13 @@ import React from "react";
 export default function Synonyms(props) {
   if (props.synonyms) {
     return (
-      <ul>
+      <ul className="Synonyms">
         {props.synonyms.map(function (synonym, index) {
           return <li key={index}>{synonym}</li>;
         })}
       </ul>
     );
+  } else {
+    return null;
   }
 }
